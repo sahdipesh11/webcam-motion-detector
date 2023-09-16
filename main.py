f21f10352864b536aadc0b86032fd58e47a576bd
sahd11 <sahdipesh11@gmail.com>
2023-09-16 23:10:05 +1000

    Extract an image from the list of images that are taken from the webcam to send in an email

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import time
 from emailing import send_email
+import glob
 
 video = cv2.VideoCapture(1)
 # Wait 1 second for the camera to load
@@ -8,9 +9,12 @@ time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
+
 while True:
     status = 0
     check, frame = video.read()
+
     # Convert the frame to gray color and then blur.
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
@@ -23,7 +27,7 @@ while True:
     delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
 
     # Classify deltaframe pixels (pixels with 30 or higher to 255)
-    thresh_frame = cv2.threshold(delta_frame, 80, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
     dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
     cv2.imshow("My video", dil_frame)
 
@@ -38,13 +42,18 @@ while True:
         rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            # Get the image that is in the middle of the list
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
 
     status_list.append(status)
     status_list = status_list[-2:]
 
-    if status_list[0] ==1 and status_list[1] == 0:
+    if status_list[0] == 1 and status_list[1] == 0:
         send_email()
-
 
     cv2.imshow("Video", frame)
 
